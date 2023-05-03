8cb169d5e82e9b3972567afe4579671da7b47cff
hiamthach <dreon1008@gmail.com>
2023-05-03 10:21:54 +0700

    fix eslint and prettier and build vue project

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,7 @@
-<script setup lang="ts">
-import HelloWorld from '@/components/HelloWorld.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div>
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 import {defineConfig} from "vite";
+import eslintPlugin from "vite-plugin-eslint";
+import prettierPlugin from "vite-plugin-prettier";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +11,11 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    eslintPlugin({
+      include: ["src/**/*.vue", "src/**/*.js"],
+    }),
+    prettierPlugin(),
+  ],
 });
